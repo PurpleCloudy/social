2fc14aedb0cd216bd8d95b20f23166ad7c743a4f
Ilya <forstudy546@gmail.com>
2023-05-25 23:01:03 +0300

    added urls of changing passwords

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,11 +5,21 @@ from . import views
 urlpatterns = [
     # path('login/', views.user_login, name = 'login')
 
+    # url входа и выхода
     path('login/', auth_views.LoginView.as_view(), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
 
+    # url смены пароля
     path('password-change/', auth_views.PasswordChangeView.as_view(), name = 'password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name = 'password_change_done'), 
 
+    # url сброса пароля
+    path('paswword-reset/', auth_views.PasswordResetView.as_view(), name = 'password-reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
+    path('password_reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'),
+
     path('', views.dashboard, name = 'dashboard'),
+
+
 ]
